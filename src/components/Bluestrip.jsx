23c002608a1c9877b51eb24ddd4e8f5ec8589a1c
MegaEmme <mikmusco@gmail.com>
2025-04-03 21:43:32 +0200

    feat: fixed z-index in footer list

--- src/components/Bluestrip.jsx
+++ src/components/Bluestrip.jsx
@@ -1,24 +1,34 @@
 function Bluestrip() {
     return <div className="blue-strip">
         <div className="strip-object">
-            <div className="strip-image strip-img-background-one"></div>
-            <span>DIGITAL COMICS</span>
+            <a href="#">
+                <div className="strip-image strip-img-background-one"></div>
+                <span>DIGITAL COMICS</span>
+            </a>
         </div>
         <div className="strip-object">
-            <div className="strip-image strip-img-background-two"></div>
-            <span>DC MERCHANDISE</span>
+            <a href="#">
+                <div className="strip-image strip-img-background-two"></div>
+                <span>DC MERCHANDISE</span>
+            </a>
         </div>
         <div className="strip-object">
-            <div className="strip-image strip-img-background-three"></div>
-            <span>SUBSCRIPTION</span>
+            <a href="#">
+                <div className="strip-image strip-img-background-three"></div>
+                <span>SUBSCRIPTION</span>
+            </a>
         </div>
         <div className="strip-object">
-            <div className="strip-image strip-img-background-four"></div>
-            <span>COMIC SHOP LOCATOR</span>
+            <a href="#">
+                <div className="strip-image strip-img-background-four"></div>
+                <span>COMIC SHOP LOCATOR</span>
+            </a>
         </div>
         <div className="strip-object">
-            <div className="strip-image strip-img-background-five"></div>
-            <span>DC POWER VISA</span>
+            <a href="#">
+                <div className="strip-image strip-img-background-five"></div>
+                <span>DC POWER VISA</span>
+            </a>
         </div>
     </div>
 }
