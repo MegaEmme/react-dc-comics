2d49fd9127290f38b9439bb052427536dbe99abe
MegaEmme <mikmusco@gmail.com>
2025-04-03 16:03:57 +0200

    feat: fix header margins

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 function Footer() {
     return <footer>
-        <div className="footer-top"></div>
+        <div className="footer-top">
+            <div className="footer-top-left"></div>
+            <div className="footer-top-right"></div>
+        </div>
         <div className="footer-bottom"></div>
     </footer>
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 function Main() {
     return <main>
-        <h1>-- Content goes here --</h1>
+        <span>-- Content goes here --</span>
     </main>
 }
 
