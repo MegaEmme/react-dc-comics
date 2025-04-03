fd43620fd1d53d449c0da13ca7b299857c3e7f42
MegaEmme <mikmusco@gmail.com>
2025-04-03 16:57:33 +0200

    feat: working on footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,57 @@
 function Footer() {
     return <footer>
         <div className="footer-top">
-            <div className="footer-top-left"></div>
+            <div className="footer-top-left">
+                <ul className="footer-list">
+                    <li>DC COMICS
+                        <ul className="footer-list-inner">
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                        </ul>
+                    </li>
+                    <li>SHOP
+                        <ul className="footer-list-inner">
+                            <li>ciao</li>
+                            <li>ciao</li>
+                        </ul>
+                    </li>
+                    <li>DC
+                        <ul className="footer-list-inner">
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy (New)</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                        </ul>
+                    </li>
+                    <li>SITES
+                        <ul className="footer-list-inner">
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                            <li>ciao</li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
             <div className="footer-top-right"></div>
         </div>
-        <div className="footer-bottom"></div>
+        <div className="footer-bottom">
+            <div className="footer-bottom-left"></div>
+            <div className="footer-bottom-right"></div>
+        </div>
     </footer>
 }
 
