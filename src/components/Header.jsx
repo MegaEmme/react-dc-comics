b85603f7c438ab8cf700918f45f37649a406f5d4
MegaEmme <mikmusco@gmail.com>
2025-04-07 17:40:08 +0200

    feat: create map. on navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,27 @@
 import Logo from "./Logo";
 import Menu from "./Menu";
 
-const MenuVoices =
-    ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"];
+// const MenuVoices =
+//     ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"];
+
+const MenuVoices = [
+    {
+        id: "1",
+        title: "CHARACTERS",
+        site: "http://www.dc.com/characters"
+    },
+    {
+        id: "2",
+        title: "COMICS",
+        site: "http://www.dc.com/comics"
+    },
+    {
+        id: "3",
+        title: "MOVIES",
+        site: "http://www.dc.com/movies"
+    }
+]
+
 
 function Header() {
     return <header>
@@ -16,3 +35,4 @@ function Header() {
 }
 
 export default Header;
+
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,14 @@
 function Menu(props) {
     const menu = props.MenuVoices;
-    return <ul className="menu-list">
-        <li><a href="http://www.dc.com/characters" target="_blank">{menu[0]}</a></li>
-        <li><a href="http://www.dc.com/comics" target="_blank">{menu[1]}</a></li>
-        <li><a href="http://www.dc.com/movies" target="_blank">{menu[2]}</a></li>
-        <li><a href="http://www.dc.com/tv" target="_blank">{menu[3]}</a></li>
-        <li><a href="http://www.dc.com/games" target="_blank">{menu[4]}</a></li>
-        <li><a href="https://shop.dc.com/collections/logo-shop" target="_blank">{menu[5]}</a></li>
-        <li><a href="http://www.dc.com/videos" target="_blank">{menu[6]}</a></li>
-        <li><a href="https://discord.com/servers/dc-official-1253043746090520626" target="_blank">{menu[7]}</a></li>
-        <li><a href="http://www.dc.com/news" target="_blank">{menu[8]}</a></li>
-        <li><a href="http://shop.dc.com" target="_blank">{menu[9]}</a></li>
-    </ul>
+    return (
+        <>
+            {menu.map(param =>
+                <ul className="menu-list" key={param.id}>
+                    <li><a href={param.site} target="_blank">{param.title}</a></li>
+                </ul>
+            )}
+        </>
+    )
 }
 
 export default Menu;
