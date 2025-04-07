f345d7401aba45f8b8344d2e0a3ad6db9adcbb6a
MegaEmme <mikmusco@gmail.com>
2025-04-07 17:51:44 +0200

    feat: closed navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,6 @@
 import Logo from "./Logo";
 import Menu from "./Menu";
 
-// const MenuVoices =
-//     ["CHARACTERS", "COMICS", "MOVIES", "TV", "GAMES", "COLLECTIBLES", "VIDEOS", "FANS", "NEWS", "SHOP"];
-
 const MenuVoices = [
     {
         id: "1",
@@ -19,6 +16,41 @@ const MenuVoices = [
         id: "3",
         title: "MOVIES",
         site: "http://www.dc.com/movies"
+    },
+    {
+        id: "4",
+        title: "TV",
+        site: "http://www.dc.com/tv"
+    },
+    {
+        id: "5",
+        title: "GAMES",
+        site: "http://www.dc.com/games"
+    },
+    {
+        id: "6",
+        title: "COLLECTIBLES",
+        site: "https://shop.dc.com/collections/logo-shop"
+    },
+    {
+        id: "7",
+        title: "VIDEOS",
+        site: "http://www.dc.com/videos"
+    },
+    {
+        id: "8",
+        title: "FANS",
+        site: "https://discord.com/servers/dc-official-1253043746090520626"
+    },
+    {
+        id: "9",
+        title: "NEWS",
+        site: "http://www.dc.com/news"
+    },
+    {
+        id: "10",
+        title: "SHOP",
+        site: "http://shop.dc.com"
     }
 ]
 
